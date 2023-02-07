76fcc0538779d0db8b175da5e211a82fbb8a4f2b
dread <dreadcreator5@gmail.com>
2023-02-07 13:26:28 +0530

    Initial Commit: Get Zapped!
    Signed-off-by: dread <dreadcreator5@gmail.com>

---
+++ src/com/zapped/settings/fragments/LockScreenSettings.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2017-2019 The PixelDust Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zapped.settings.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.zapped.settings.preferences.SystemSettingListPreference;
+
+import java.lang.StringBuilder;
+
+@SearchIndexable
+public class LockScreenSettings extends DashboardFragment
+        implements OnPreferenceChangeListener {
+
+    private static final String TAG = "LockScreenSettings";
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
+    private static final String SHORTCUT_START_KEY = "lockscreen_shortcut_start";
+    private static final String SHORTCUT_END_KEY = "lockscreen_shortcut_end";
+    private static final String SHORTCUT_ENFORCE_KEY = "lockscreen_shortcut_enforce";
+
+    private static final String[] DEFAULT_START_SHORTCUT = new String[] { "home", "flashlight" };
+    private static final String[] DEFAULT_END_SHORTCUT = new String[] { "wallet", "qr", "camera" };
+
+    static final int MODE_DISABLED = 0;
+    static final int MODE_NIGHT = 1;
+    static final int MODE_TIME = 2;
+    static final int MODE_MIXED_SUNSET = 3;
+    static final int MODE_MIXED_SUNRISE = 4;
+
+    private Preference mAODPref;
+    private SystemSettingListPreference mStartShortcut;
+    private SystemSettingListPreference mEndShortcut;
+    private SwitchPreference mEnforceShortcut;
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.zap_settings_lockscreen;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        updateAlwaysOnSummary();
+
+        mStartShortcut = findPreference(SHORTCUT_START_KEY);
+        mEndShortcut = findPreference(SHORTCUT_END_KEY);
+        mEnforceShortcut = findPreference(SHORTCUT_ENFORCE_KEY);
+        updateShortcutSelection();
+        mStartShortcut.setOnPreferenceChangeListener(this);
+        mEndShortcut.setOnPreferenceChangeListener(this);
+        mEnforceShortcut.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAlwaysOnSummary();
+        updateShortcutSelection();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mStartShortcut) {
+            setShortcutSelection((String) objValue, true);
+            return true;
+        } else if (preference == mEndShortcut) {
+            setShortcutSelection((String) objValue, false);
+            return true;
+        } else if (preference == mEnforceShortcut) {
+            final boolean value = (Boolean) objValue;
+            setShortcutSelection(mStartShortcut.getValue(), true, value);
+            setShortcutSelection(mEndShortcut.getValue(), false, value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            default:
+            case MODE_DISABLED:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case MODE_NIGHT:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case MODE_TIME:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
+            case MODE_MIXED_SUNSET:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunset);
+                break;
+            case MODE_MIXED_SUNRISE:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunrise);
+                break;
+        }
+    }
+
+    private String getSettingsShortcutValue() {
+        String value = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.KEYGUARD_QUICK_TOGGLES);
+        if (value == null || value.isEmpty()) {
+            StringBuilder sb = new StringBuilder(DEFAULT_START_SHORTCUT[0]);
+            for (int i = 1; i < DEFAULT_START_SHORTCUT.length; i++) {
+                sb.append(",").append(DEFAULT_START_SHORTCUT[i]);
+            }
+            sb.append(";" + DEFAULT_END_SHORTCUT[0]);
+            for (int i = 1; i < DEFAULT_END_SHORTCUT.length; i++) {
+                sb.append(",").append(DEFAULT_END_SHORTCUT[i]);
+            }
+            value = sb.toString();
+        }
+        return value;
+    }
+
+    private void updateShortcutSelection() {
+        final String value = getSettingsShortcutValue();
+        final String[] split = value.split(";");
+        final String[] start = split[0].split(",");
+        final String[] end = split[1].split(",");
+        mStartShortcut.setValue(start[0]);
+        mStartShortcut.setSummary(mStartShortcut.getEntry());
+        mEndShortcut.setValue(end[0]);
+        mEndShortcut.setSummary(mEndShortcut.getEntry());
+        mEnforceShortcut.setChecked(start.length == 1 && end.length == 1);
+    }
+
+    private void setShortcutSelection(String value, boolean start) {
+        setShortcutSelection(value, start, mEnforceShortcut.isChecked());
+    }
+
+    private void setShortcutSelection(String value, boolean start, boolean single) {
+        final String oldValue = getSettingsShortcutValue();
+        final int splitIndex = start ? 0 : 1;
+        String[] split = oldValue.split(";");
+        if (value.equals("none") || single) {
+            split[splitIndex] = value;
+        } else {
+            StringBuilder sb = new StringBuilder(value);
+            final String[] def = start ? DEFAULT_START_SHORTCUT : DEFAULT_END_SHORTCUT;
+            for (String str : def) {
+                if (str.equals(value)) continue;
+                sb.append(",").append(str);
+            }
+            split[splitIndex] = sb.toString();
+        }
+        Settings.System.putString(getActivity().getContentResolver(),
+                Settings.System.KEYGUARD_QUICK_TOGGLES, split[0] + ";" + split[1]);
+
+        if (start) {
+            mStartShortcut.setValue(value);
+            mStartShortcut.setSummary(mStartShortcut.getEntry());
+        } else {
+            mEndShortcut.setValue(value);
+            mEndShortcut.setSummary(mEndShortcut.getEntry());
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ZAPPED;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.zap_settings_lockscreen);
+}
---
+++ src/com/zapped/settings/fragments/NotificationsSettings.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2017-2019 The PixelDust Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zapped.settings.fragments;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.zap.ZapUtils;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.zapped.settings.preferences.CustomSeekBarPreference;
+import com.zapped.settings.preferences.SystemSettingListPreference;
+import com.zapped.settings.preferences.SystemSettingMasterSwitchPreference;
+import com.zapped.settings.preferences.SystemSettingSwitchPreference;
+import com.zapped.settings.Utils;
+
+@SearchIndexable
+public class NotificationsSettings extends DashboardFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "NotificationsSettings";
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String FLASH_ON_CALL_OPTIONS = "on_call_flashlight_category";
+    private static final String FLASH_ON_NOTIFICATION_OPTIONS = "notification_flashlight_category";
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
+    private static final String PREF_FLASH_ON_NOTIFY = "default_notification_torch";
+    private static final String PREF_FLASH_ON_NOTIFY_TIMES = "default_notification_torch1";
+    private static final String PREF_FLASH_ON_NOTIFY_RATE = "default_notification_torch2";
+    private static final String KEY_BATT_LIGHT = "battery_light_enabled";
+    private static final String KEY_EDGE_LIGHTNING = "pulse_ambient_light";
+
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private CustomSeekBarPreference mFlashOnCallRate;
+    private SwitchPreference mFlashOnNotify;
+    private CustomSeekBarPreference mFlashOnNotifyTimes;
+    private CustomSeekBarPreference mFlashOnNotifyRate;
+    private SystemSettingMasterSwitchPreference mBatteryLight;
+    private SystemSettingMasterSwitchPreference mEdgeLightning;
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.zap_settings_notifications;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
+
+        if (!ZapUtils.deviceHasFlashlight(getActivity())) {
+            PreferenceCategory flashOnCallCategory = (PreferenceCategory)
+                    findPreference(FLASH_ON_CALL_OPTIONS);
+            PreferenceCategory flashOnNotifCategory = (PreferenceCategory)
+                    findPreference(FLASH_ON_NOTIFICATION_OPTIONS);
+            prefScreen.removePreference(flashOnCallCategory);
+            prefScreen.removePreference(flashOnNotifCategory);
+        } else {
+            mFlashOnCallRate = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_CALL_RATE);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+            mFlashOnCallRate.setValue(value);
+            mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                    findPreference(PREF_FLASH_ON_CALL_DND);
+            value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+
+            mFlashOnCall = (SystemSettingListPreference)
+                    findPreference(PREF_FLASH_ON_CALL);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+
+            mFlashOnNotifyTimes = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_NOTIFY_TIMES);
+            mFlashOnNotifyRate = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_NOTIFY_RATE);
+            mFlashOnNotify = (SwitchPreference)
+                    findPreference(PREF_FLASH_ON_NOTIFY);
+            String strVal = Settings.System.getStringForUser(resolver,
+                    PREF_FLASH_ON_NOTIFY, UserHandle.USER_CURRENT);
+            final boolean enabled = strVal != null && !strVal.isEmpty();
+            mFlashOnNotify.setChecked(enabled);
+            updateFlashOnNotifyValues(enabled, strVal);
+            mFlashOnNotify.setOnPreferenceChangeListener(this);
+            mFlashOnNotifyTimes.setOnPreferenceChangeListener(this);
+            mFlashOnNotifyRate.setOnPreferenceChangeListener(this);
+        }
+
+        mBatteryLight = (SystemSettingMasterSwitchPreference)
+                findPreference(KEY_BATT_LIGHT);
+        boolean hasLed = getResources().getBoolean(
+                com.android.internal.R.bool.config_hasNotificationLed);
+        if (hasLed) {
+            boolean enabled = Settings.System.getInt(resolver,
+                    KEY_BATT_LIGHT, 1) == 1;
+            mBatteryLight.setChecked(enabled);
+            mBatteryLight.setOnPreferenceChangeListener(this);
+        } else {
+            mBatteryLight.setVisible(false);
+        }
+
+        mEdgeLightning = (SystemSettingMasterSwitchPreference)
+                findPreference(KEY_EDGE_LIGHTNING);
+        boolean enabled = Settings.System.getIntForUser(resolver,
+                KEY_EDGE_LIGHTNING, 0, UserHandle.USER_CURRENT) == 1;
+        mEdgeLightning.setChecked(enabled);
+        mEdgeLightning.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
+            return true;
+        } else if (preference == mBatteryLight) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, KEY_BATT_LIGHT, value ? 1 : 0);
+            return true;
+        } else if (preference == mEdgeLightning) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(resolver, KEY_EDGE_LIGHTNING,
+                    value ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mFlashOnNotify) {
+            boolean value = (Boolean) newValue;
+            if (!value) setFlashOnNotifyValues(0, 0);
+            else setFlashOnNotifyValues(2, 2);
+            return true;
+        } else if (preference == mFlashOnNotifyTimes) {
+            int value = (Integer) newValue;
+            setFlashOnNotifyValues(value, mFlashOnNotifyRate.getValue());
+            return true;
+        } else if (preference == mFlashOnNotifyRate) {
+            int value = (Integer) newValue;
+            setFlashOnNotifyValues(mFlashOnNotifyTimes.getValue(), value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateFlashOnNotifyValues(boolean enabled) {
+        final String val = Settings.System.getStringForUser(
+                getActivity().getContentResolver(),
+                PREF_FLASH_ON_NOTIFY, UserHandle.USER_CURRENT);
+        updateFlashOnNotifyValues(enabled, val);
+    }
+
+    private void updateFlashOnNotifyValues(boolean enabled, String val) {
+        if (enabled) {
+            if (val.equals("1")) {
+                mFlashOnNotifyTimes.setValue(2);
+                mFlashOnNotifyRate.setValue(2);
+            } else {
+                String[] vals = val.split(",");
+                mFlashOnNotifyTimes.setValue(Integer.valueOf(vals[0]));
+                mFlashOnNotifyRate.setValue(Integer.valueOf(vals[1]));
+            }
+        }
+        mFlashOnNotifyTimes.setVisible(enabled);
+        mFlashOnNotifyRate.setVisible(enabled);
+    }
+
+    private void setFlashOnNotifyValues(int times, int rate) {
+        final boolean enabled = times != 0 && rate != 0;
+        String val = String.valueOf(times) + "," + String.valueOf(rate);
+        if (times == 2 && rate == 2) val = "1";
+        Settings.System.putStringForUser(getActivity().getContentResolver(),
+                PREF_FLASH_ON_NOTIFY, enabled ? val : null, UserHandle.USER_CURRENT);
+        updateFlashOnNotifyValues(enabled, val);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ZAPPED;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.zap_settings_notifications);
+}
